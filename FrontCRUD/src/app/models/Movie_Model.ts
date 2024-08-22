29e3821452883f7f659b83d218e64a8be584d263
Habba <frannagu03@hotmail.com>
2024-08-22 00:27:11 -0600

    0.35

--- FrontCRUD/src/app/models/Movie_Model.ts
+++
@@ -1,9 +0,0 @@
-export interface Movie {
-    pkMovies: number;
-    name: string;
-    gender: string;
-    duration: string;
-
-    fkDirector: number;
-    director: number;
-}
--- FrontCRUD/src/app/models/Movie_Up_Model.ts
+++
@@ -1,7 +0,0 @@
-export interface MovieUpload {
-    name: string;
-    gender: string;
-    duration: string;
-
-    fKDirector: number;
-}
